d55701645cfe2d2a921bc542876ecef86c4b2695
Jweiler94 <jweiler94@hotmail.com>
2023-08-10 22:57:44 -0600

    commit before testing

---
+++ src/components/Banner.js
@@ -0,0 +1,9 @@
+export default function Banner() {
+      return(
+            <div>
+                  <header>Orbit Report</header>
+                  <p>Click on the buttons to see the satellites by Orbit Type!</p>
+            </div>
+      );
+}
+
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,16 +1,29 @@
-const Table = ({ sat }) => {
+
+
+const Table = (props) => {
+  let opsCheck;
   return (
       <table>
        <thead>
         <tr>
-          <th>Header TBD</th>
+          <th>Name</th>
+          <th>Type of Satellite</th>
+          <th>Launch Date</th>
+          <th>Status</th>
         </tr>
         </thead>
         <tbody>
-        <tr>
-          <td>Row Data TBD</td>
-        </tr>
-        </tbody>
+          {props.sat.map((data, id) => {if(data.operational === true) {opsCheck = "Active";} else {opsCheck = "Inactive";}
+            return (
+              <tr key={id}>
+                <td>{data.name}</td>
+                <td>{data.type}</td>
+                <td>{data.launchDate}</td>
+                <td>{opsCheck}</td>
+            </tr>
+            )
+          })}
+  </tbody>
       </table>
   );
 };
